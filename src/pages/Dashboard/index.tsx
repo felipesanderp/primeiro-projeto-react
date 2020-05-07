b2da41a8e0ead882e1d60cfda0a025b0c7f6a53a
felipesanderp <felipe.panisson@hotmail.com>
2020-05-06 23:08:53 -0300

    Add styles to dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="Github Explorer" />
+    <Title>Explore Repositórios do Github.</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars2.githubusercontent.com/u/46326536?s=460&u=f72b94042e77899f6c4df3c5f41c9984456cda87&v=4"
+          alt="Felipe Sander"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+        </div>
+
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
+
+export default Dashboard;
